bfe42b375e484fd689a087bb76df2fd5892f6627
YepJared <97353828+YepJared@users.noreply.github.com>
2024-11-07 14:45:06 -0500

    Add logic to play out a single round of rock, paper, scissors
    Update playRound function to determine victory condition
    Add determineWinner function to determine if human or computer wins when not a draw
    Add displayRoundResolution function to increment the score based on the winner and then display victory messaging
    Fix bug in validateHumanChoice function where new prompts would not return to a victory condition
    Update psuedocode to reflect score updating before any messaging to the player

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // 2. Prompt human for choice of rps
 // 3. Get human choice of rps as string
 // 4. Compare human choice to computer choice
-// 5. Return text of victory, defeat, or draw
-// 6. Update human or player score
+// 5. Update human or player score
+// 6. Return text of victory, defeat, or draw
 // 7. Return text of current score comparison
 // 8. Prompt player for choice again 
 
@@ -23,16 +23,13 @@ function validateHumanChoice(rawText) {
     let lowerCaseText = (rawText != null) ? rawText.toLowerCase() : ""
     switch(lowerCaseText) {
         case "":
-            validateHumanChoice(prompt("Please enter a value. Rock, paper, or scissors?"));
-            break;
+            return validateHumanChoice(prompt("Please enter a value. Rock, paper, or scissors?"));
         case choiceMap.get(0):
         case choiceMap.get(1):
         case choiceMap.get(2):
             return lowerCaseText;
         default:
-            validateHumanChoice(prompt("Please enter a valid value. Rock, paper or scissors?"));
-            break;
-        
+            return validateHumanChoice(prompt("Please enter a valid value. Rock, paper or scissors?"));
     }
 } 
 
@@ -42,7 +39,50 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    // code for play to go here
+    if (humanChoice === computerChoice) {
+        console.log(`A draw. You both chose ${humanChoice}`);
+        console.log(`The current score is human: ${humanScore}, computer: ${computerScore}.`);
+    } else {
+        determineWinner(humanChoice, computerChoice);
+    }
+}
+
+function determineWinner(humanChoice, computerChoice) {
+    switch(humanChoice) {
+        case choiceMap.get(0):
+            if (computerChoice === choiceMap.get(1)) {
+                displayRoundResolution(false, humanChoice, computerChoice);
+            } else {
+                displayRoundResolution(true, humanChoice, computerChoice);
+            }
+            break;
+        case choiceMap.get(1):
+            if (computerChoice === choiceMap.get(2)) {
+                displayRoundResolution(false, humanChoice, computerChoice);
+            } else {
+                displayRoundResolution(true, humanChoice, computerChoice);
+            }
+            break;
+        case choiceMap.get(2):
+            if (computerChoice === choiceMap.get(0)) {
+                displayRoundResolution(false, humanChoice, computerChoice);
+            } else {
+                displayRoundResolution(true, humanChoice, computerChoice);
+            }
+            break;
+    }
+}
+
+function displayRoundResolution(humanWins, humanChoice, computerChoice) {
+    if (humanWins === true) {
+        humanScore++;
+        console.log(`Glorious victory! You chose ${humanChoice} and the computer chose ${computerChoice}.`);
+        console.log(`The current score is human: ${humanScore}, computer: ${computerScore}.`);
+    } else if (humanWins === false) {
+        computerScore++;
+        console.log(`Defeat! You chose ${humanChoice} and the computer chose ${computerChoice}.`);
+        console.log(`The current score is human: ${humanScore}, computer: ${computerScore}.`);
+    }
 }
 
 let computerChoice = getComputerChoice();
