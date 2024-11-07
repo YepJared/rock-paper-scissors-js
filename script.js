553e7e8714e4af57e382761319c86c722585f3ec
YepJared <97353828+YepJared@users.noreply.github.com>
2024-11-06 19:59:35 -0500

    Add initial algo pseudocode and function to get the computer's choice
    Create pseudocode for individual rounds against the computer
    Add logic to generate random number between 0 and 2 inclusive
    Add logic to convert integer to choice of rock, paper, scissors string

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello")
+// Algorithm pseudocode
+// 1. Create computer choice of rock, paper, or scissors (rps)
+// 2. Prompt human for choice of rps
+// 3. Get human choice of rps as string
+// 4. Compare human choice to computer choice
+// 5. Return text of victory, defeat, or draw
+// 6. Update human or player score
+// 7. Return text of current score comparison
+// 8. Prompt player for choice again 
+
+function getComputerChoice() {
+    let choiceNumber = Math.floor(Math.random() * 100) % 3;
+    if (choiceNumber === 0) {
+        return "rock"
+    } else if (choiceNumber === 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
